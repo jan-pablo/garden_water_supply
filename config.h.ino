e99e6a54206bdf0c5bf086c99dc995c26439aea7
jan-pablo <69260986+jan-pablo@users.noreply.github.com>
2020-09-21 14:34:20 +0200

    Added Config.h.ino
    dummy config.h.ino with empty host, ssid, password

---
+++ config.h.ino
@@ -0,0 +1,6 @@
+//WLAN infos
+const char* WLAN_host = "192.168.xyz.xyz";      // add IP of MQTT Server
+
+// WiFi settings
+const char* WIFI_ssid = "user_ssid";            // add your WLAN ssid
+const char* WIFI_password = "user_password";    // add your WLAN ssid
