bbc27e94e4fdaeccd7c9c5ed8dd9af04a26ab5ba
jan-pablo <69260986+jan-pablo@users.noreply.github.com>
2020-10-29 17:30:54 +0100

    Update config.h.ino

--- config.h.ino
+++ config.h.ino
@@ -3,4 +3,4 @@ const char* WLAN_host = "192.168.xyz.xyz";      // add IP of MQTT Server
 
 // WiFi settings
 const char* WIFI_ssid = "user_ssid";            // add your WLAN ssid
-const char* WIFI_password = "user_password";    // add your WLAN ssid
+const char* WIFI_password = "user_password";    // add your WLAN password
